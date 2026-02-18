832c1237e44813b5b1cb16903fb7942cfc23f006
Saanvi Sharma <saanvi.s019@gmail.com>
2026-02-18 11:47:13 +0530

    implemented auth control

---
+++ server/controllers/authController.js
@@ -0,0 +1,73 @@
+import User from "../models/user";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+export const signUp= async (req,res)=>{
+    try {
+        const {name,email,password}=req.body;
+
+        const existingUser=await User.findOne({email});
+        if(existingUser){
+            return res.status(400).res.json({message:"email already in use"});
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword= await bcrypt.hash(password,salt);
+
+        const user= await User.create({
+            name,
+            email,
+            password:hashedPassword,
+        })
+
+        res.status(201).json({
+            message:"user created successfully",
+            userId:user._id,
+        })
+    } catch (error) {
+        res.status(500).json({
+            message:"signup failed",
+            error:error.message,
+        })
+    }
+}
+
+export const login= async (req,res)=>{
+    try {
+        const {email, password}=req.body;
+
+        const user=await User.findOne({email});
+        if(!user){
+            res.status(400).json({message:"Invalid Credentials"});
+        }
+
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            res.status(400).json({message:"invalid credentials"});
+        }
+
+        const token = jwt.sign(
+            {id:user._id},
+            process.env.JWT_SECRET,
+            {expiresIn:"7d"}
+        );
+
+        res.status(200).json({
+            message:"login successful",
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email,
+            },
+        });
+
+        
+    } catch (error) {
+        res.status(500).json({
+            message:"login failed",
+            error:error.message
+        })
+    }
+}
+
