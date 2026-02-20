73205fb123373ed06d4aa86f1e6d8ba01e4d536b
Saanvi Sharma <saanvi.s019@gmail.com>
2026-02-20 15:03:30 +0530

    implemented routes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Error:", err));
 
 const app=express()
 const port=process.env.PORT;
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth",authRoutes);
 
 app.get('/',(req,res)=>{
     res.send("port rumming")
